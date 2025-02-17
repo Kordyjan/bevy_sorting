8a0f4fc1a4d90b83e247a3f97d9a4bd83b1e272b
Paweł Marks <paweljmarks@gmail.com>
2025-02-17 14:17:06 +0100

    Implement sorting logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,130 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::{
+    any::{type_name, TypeId},
+    fmt::{self, Debug, Formatter},
+    hash::{Hash, Hasher},
+    marker::PhantomData,
+};
+
+use bevy::{
+    ecs::{intern::Interned, label, schedule::NodeConfigs},
+    prelude::{IntoSystemConfigs, IntoSystemSetConfigs, System, SystemSet},
+};
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct Reads<T: 'static>(PhantomData<fn() -> T>);
+
+impl<T: 'static> Default for Reads<T> {
+    fn default() -> Self {
+        Self(PhantomData)
     }
+}
+
+impl<T> Debug for Reads<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("Reads").field(&type_name::<T>()).finish()
+    }
+}
+
+impl<T> Hash for Reads<T> {
+    fn hash<H: Hasher>(&self, _state: &mut H) {}
+}
+
+impl<T> Clone for Reads<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Copy for Reads<T> {}
+
+impl<T> PartialEq for Reads<T> {
+    fn eq(&self, _other: &Self) -> bool {
+        true
+    }
+}
+
+impl<T> Eq for Reads<T> {}
+
+impl<T> SystemSet for Reads<T> {
+    fn dyn_clone(&self) -> Box<dyn SystemSet> {
+        Box::new(*self)
+    }
+
+    fn as_dyn_eq(&self) -> &dyn label::DynEq {
+        self
+    }
+
+    fn dyn_hash(&self, mut state: &mut dyn Hasher) {
+        TypeId::of::<Self>().hash(&mut state);
+    }
+}
+
+pub struct Writes<T: 'static>(PhantomData<fn() -> T>);
+
+impl<T: 'static> Default for Writes<T> {
+    fn default() -> Self {
+        Self(PhantomData)
+    }
+}
+
+impl<T> Debug for Writes<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("Writes").field(&type_name::<T>()).finish()
+    }
+}
+
+impl<T> Hash for Writes<T> {
+    fn hash<H: Hasher>(&self, _state: &mut H) {}
+}
+
+impl<T> Clone for Writes<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Copy for Writes<T> {}
+
+impl<T> PartialEq for Writes<T> {
+    fn eq(&self, _other: &Self) -> bool {
+        true
+    }
+}
+
+impl<T> Eq for Writes<T> {}
+
+impl<T> SystemSet for Writes<T> {
+    fn dyn_clone(&self) -> Box<dyn SystemSet> {
+        Box::new(*self)
+    }
+
+    fn as_dyn_eq(&self) -> &dyn label::DynEq {
+        self
+    }
+
+    fn dyn_hash(&self, mut state: &mut dyn Hasher) {
+        TypeId::of::<Self>().hash(&mut state);
+    }
+}
+
+pub trait IntoSystemRW<M>: IntoSystemConfigs<M> {
+    fn reads<T: 'static>(self) -> NodeConfigs<Box<dyn System<In = (), Out = ()>>> {
+        self.in_set(Reads::<T>::default())
+    }
+
+    fn writes<T: 'static>(self) -> NodeConfigs<Box<dyn System<In = (), Out = ()>>> {
+        self.in_set(Writes::<T>::default())
+    }
+}
+
+impl<M, S: IntoSystemConfigs<M>> IntoSystemRW<M> for S {}
+
+pub fn write_before_read<T: 'static>() -> NodeConfigs<Interned<dyn SystemSet>> {
+    Writes::<T>::default().before(Reads::<T>::default())
+}
+
+pub fn read_before_write<T: 'static>() -> NodeConfigs<Interned<dyn SystemSet>> {
+    Reads::<T>::default().before(Writes::<T>::default())
 }
---
+++ src/tests.rs
@@ -0,0 +1,124 @@
+use std::sync::{Arc, Mutex};
+
+use crate::*;
+use bevy::prelude::*;
+
+#[test]
+fn reads_before_writes_test() {
+    for _ in 0..8 {
+        let trace = Arc::new(Mutex::new(Vec::<String>::new()));
+
+        let mut app = App::new();
+
+        let trace_handle = Arc::clone(&trace);
+        app.add_systems(
+            Startup,
+            (move || {
+                trace_handle.lock().unwrap().push("2".to_string());
+            })
+            .writes::<Marker>(),
+        );
+
+        let trace_handle = Arc::clone(&trace);
+        app.add_systems(
+            Startup,
+            (move || {
+                trace_handle.lock().unwrap().push("1".to_string());
+            })
+            .reads::<Marker>(),
+        );
+
+        app.configure_sets(Startup, read_before_write::<Marker>());
+
+        app.run();
+
+        assert_eq!(*trace.lock().unwrap(), vec!["1", "2"]);
+    }
+}
+#[test]
+fn writes_before_reads_test() {
+    for _ in 0..8 {
+        let trace = Arc::new(Mutex::new(Vec::<String>::new()));
+
+        let mut app = App::new();
+
+        let trace_handle = Arc::clone(&trace);
+        app.add_systems(
+            Startup,
+            (move || {
+                trace_handle.lock().unwrap().push("1".to_string());
+            })
+            .writes::<Marker>(),
+        );
+
+        let trace_handle = Arc::clone(&trace);
+        app.add_systems(
+            Startup,
+            (move || {
+                trace_handle.lock().unwrap().push("2".to_string());
+            })
+            .reads::<Marker>(),
+        );
+
+        app.configure_sets(Startup, write_before_read::<Marker>());
+
+        app.run();
+
+        assert_eq!(*trace.lock().unwrap(), vec!["1", "2"]);
+    }
+}
+
+#[test]
+fn with_resources() {
+    for _ in 0..8 {
+        let mut app = App::new();
+
+        app.add_systems(
+            Startup,
+            (
+                zero_to_res1.writes::<Res1>(),
+                res1_to_res2a.reads::<Res1>().writes::<Res2a>(),
+                res1_to_res2b.reads::<Res1>().writes::<Res2b>(),
+                res2_to_res3.reads::<Res2a>().reads::<Res2b>(),
+            ),
+        )
+        .configure_sets(
+            Startup,
+            (
+                write_before_read::<Res1>(),
+                write_before_read::<Res2a>(),
+                write_before_read::<Res2b>(),
+            ),
+        );
+        app.run();
+    }
+}
+
+struct Marker;
+
+#[derive(Resource)]
+struct Res1;
+
+#[derive(Resource)]
+struct Res2a;
+
+#[derive(Resource)]
+struct Res2b;
+
+#[derive(Resource)]
+struct Res3;
+
+fn zero_to_res1(mut commands: Commands) {
+    commands.insert_resource(Res1);
+}
+
+fn res1_to_res2a(mut commands: Commands, _res1: Res<Res1>) {
+    commands.insert_resource(Res2a);
+}
+
+fn res1_to_res2b(mut commands: Commands, _res1: Res<Res1>) {
+    commands.insert_resource(Res2b);
+}
+fn res2_to_res3(mut commands: Commands, _res2a: Res<Res2a>, _res2b: Res<Res2b>) {
+    commands.insert_resource(Res3);
+}
