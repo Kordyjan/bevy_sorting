c40e2f765eaf372504d9a23e8c51cbec4d1d1a96
Paweł Marks <paweljmarks@gmail.com>
2025-03-01 11:19:41 +0100

    Added must use

--- src/ordering.rs
+++ src/ordering.rs
@@ -7,13 +7,13 @@ use crate::markers;
 use markers::{Reads, Writes};
 /// Constraint for `App::configure_systems` specifying that all writes to T must be executed before
 /// the first read
-pub fn write_before_read<T: 'static>() -> NodeConfigs<Interned<dyn SystemSet>> {
+#[must_use] pub fn write_before_read<T: 'static>() -> NodeConfigs<Interned<dyn SystemSet>> {
 Writes::<T>::default().before(Reads::<T>::default())
 }
 
 /// Constraint for `App::configure_systems` specifying that all reads from T must be executed
 /// before the first write
-pub fn read_before_write<T: 'static>() -> NodeConfigs<Interned<dyn SystemSet>> {
+#[must_use] pub fn read_before_write<T: 'static>() -> NodeConfigs<Interned<dyn SystemSet>> {
 Reads::<T>::default().before(Writes::<T>::default())
 }
 
